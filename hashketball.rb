81cfb42ce6ee105b56eee1922a00e3625a70efa6
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-19 17:29:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -233,6 +233,7 @@ def player_with_longest_name
   longest_name = ''
   game_hash.each do |location, team_data|
     name_length = team_data[:players][:player_name].length
+    binding.pry
     team_data[:players].each do |player|
       if points > most_points
         most_points = points
